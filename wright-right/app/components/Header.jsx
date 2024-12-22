5a00d0347e717923174a19d20d4b9a09722a1471
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-12-23 02:24:16 +0530

    header done

--- wright-right/app/components/Header.jsx
+++ wright-right/app/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Image from 'next/image'
 const header = () => {
   return (
-    <div>header</div>
+    <div className='py-5 px-5 md:px-12 lg:px-28'>
+      <div className='display-flex justify-between items-center'>
+        <Image src={assets.logo} width={180} className='w-[130px] sm:w-auto' />
+      </div>
+      </div>
   )
 }
 
