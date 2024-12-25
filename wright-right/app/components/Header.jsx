01eaf7a8cf35c44147ac1f9d77a300de91de8cf6
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-12-26 01:44:53 +0530

    initial import error resolved

--- wright-right/app/components/Header.jsx
+++ wright-right/app/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
-import { assets, blog_data } from '@/Assets/assets.js';
+import logo from '../assets/logo.png'
 const Header = () => {
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
       <div className='display-flex justify-between items-center'>
-      <Image src={assets.logo} alt='logo' width={100} height={100} />
+      <Image src={logo} alt='logo' width={120}  className='w-[130px] sm:w-auto' />
       </div>
       </div>
   )
