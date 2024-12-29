eff40d9de1f524025dbe1f5aa3e50bfe6b7828b4
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-12-29 19:47:56 +0530

    connection faced

--- wright-right/app/components/Bloglist.jsx
+++ wright-right/app/components/Bloglist.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
+import { blog_data } from '../Assets/assets'
+import Blogthings from './Blogthings'
 const Bloglist = () => {
   return (
     <div>
     <div className='flex justify-center gap-6 my-10 '>
         <button className='bg-black text-white py-1 px-4 rounded-sm'> All</button>
+         <button>Tech</button>
+          <button>Startups</button>
+           <button>life</button>
+    </div>
+    <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24 '>
+{blog_data.map((item,index) => {
+    return <Blogthings key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
+})}
     </div>
     </div>
   )
--- wright-right/app/components/Blogthings.jsx
+++ wright-right/app/components/Blogthings.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import { blog_data } from '../Assets/assets'
 import Image from 'next/image'
+import arrow from '../assets/arrow.png'
 
-const Blogthings = ({title,description,category,image}) => {
+const Blogthings = ({ title, description, category, image }) => {
   return (
-    <div className='max-w-[330px sm: max-w-[300px] bg-white border border-black hover-shadow-[7px_7px_0px_#000000]'>
-        <Image src={blog_data[0].image} alt='blog1' width={400} height={400} className=' border-b border-black' />
-          <p className='ml-5 mt-5 inline-block bg-black text-white text-sm'>{category}</p>
-          <div className='p-5'>
-            <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
-         <p className='mb-2 text-sm tracking-tight text-gray-700'>{description}</p>
-         <div className='inline-flex items-center py-2 font-semibold text-center'>
-          Read more <Image src={assets.arrow} alt='right-arrow' width={12}  className='ml-2'/>
-         </div>
-          </div>
-  </div>
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[7px_7px_0px_#000000]'>
+      <Image src={image} alt='blog1' width={400} height={400} className='border-b border-black' />
+      <p className='ml-5 mt-5 inline-block bg-black text-white text-sm'>{category}</p>
+      <div className='p-5'>
+        <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
+        <p className='mb-2 text-sm tracking-tight text-gray-700'>{description}</p>
+        <div className='inline-flex items-center py-2 font-semibold text-center'>
+          Read more <Image src={arrow} alt='right-arrow' width={12} height={12} className='ml-2' />
+        </div>
+      </div>
+    </div>
   )
 }
 
