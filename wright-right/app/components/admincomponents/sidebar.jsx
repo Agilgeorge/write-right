069a946c3f34a7bc451b2b5678f7c9a2a01d3352
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2025-01-09 00:06:58 +0530

    created necessary layouts

---
+++ wright-right/app/components/admincomponents/sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/app/Assets/assets'
+assets
+const Sidebar = () => {
+  return (
+    <div className='flex flex-col bg-slate-100'>
+        <div className='px-2 sm:pl-14 py-3 border border-black'>
+            <Image src={assets.logo} alt='logo' width={120} height={100} />
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Sidebar
